a41d7b7151832436265fa08fa065f9d8198d3a02
TomHuHMSIndustrialNetworks <yuxun.hu@gmail.com>
2025-03-20 20:21:57 +1100

    Add files via upload

---
+++ E300_Configuration_Management/ProjectFiles/NetSolution/Private/TypeConstants.cs
@@ -0,0 +1,20 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+namespace E300_Configuration_Management
+{
+    public static class ObjectTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("E300_Configuration_Management");
+        public static readonly NodeId MainWindow = new NodeId(namespaceIndex, new Guid("01a7e8933afd7c95fe894b0859308138"));
+    }
+
+    public static class VariableTypes
+    {
+        private static readonly int namespaceIndex = NamespaceMapProvider.GetNamespaceIndex("E300_Configuration_Management");
+    }
+}
---
+++ E300_Configuration_Management/ProjectFiles/NetSolution/Private/UITypeDefinitions.cs
@@ -0,0 +1,11 @@
+using System;
+using UAManagedCore;
+
+//-------------------------------------------
+// WARNING: AUTO-GENERATED CODE, DO NOT EDIT!
+//-------------------------------------------
+
+[MapType(NamespaceUri = "E300_Configuration_Management", Guid = "01a7e8933afd7c95fe894b0859308138")]
+public class MainWindow : FTOptix.UI.Window
+{
+}
